32e480cbf0bf74419bec548933c253dd44f4befb
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2021-06-30 12:33:24 +0200

    push implementation of template in laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+@section('content')
+    @include('partials.navigation.navigation')
+    @include('partials.header.header')
+    @include('partials.service.services')
+    @include('partials.portofolio.portofolio')
+    @include('partials.about.about')
+    @include('partials.teams.team')
+    @include('partials.client.client')
+    @include('partials.contact.contact')
+    @include('partials.footer.footer')
+    @include('partials.footer.footer')
+@endsection
